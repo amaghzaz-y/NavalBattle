104a83991e5bf56817345dbef9f60a1f9b84c9f8
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-10-16 10:40:22 +0200

    fix: add static modifier

--- core/src/dev/amaghzaz/navalbattle/Assets.java
+++ core/src/dev/amaghzaz/navalbattle/Assets.java
@@ -7,8 +7,8 @@ import com.badlogic.gdx.utils.Array;
 import org.w3c.dom.Text;
 
 public class Assets {
-    class Ships{
-        public Animation<Texture> VerySmall(){
+    static class  Ships{
+        public static Animation<Texture> VerySmall(){
             Texture frame_1 =  new Texture(Gdx.files.internal("Boats/Boat1_water_frame1.png"));
             Texture frame_2 =  new Texture(Gdx.files.internal("Boats/Boat1_water_frame2.png"));
             Texture frame_3 =  new Texture(Gdx.files.internal("Boats/Boat1_water_frame3.png"));
@@ -48,6 +48,5 @@ public class Assets {
         Texture[] animations = new Texture[]{frame_1, frame_2, frame_3};
         return new Animation<>(0.025f, animations);
     }
-
 }
 
--- core/src/dev/amaghzaz/navalbattle/Ship.java
+++ core/src/dev/amaghzaz/navalbattle/Ship.java
@@ -2,10 +2,13 @@ package dev.amaghzaz.navalbattle;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+
+import java.util.HashSet;
 
 public class Ship extends Sprite {
 
@@ -20,6 +23,8 @@ public class Ship extends Sprite {
         Medium,
         Big
     }
+    float stateTime = 0.0F;
+    Animation<Texture> animation;
     private final Texture texture;
     private int hitpoints = 0;
     private Vector2 position = new Vector2();
@@ -31,17 +36,26 @@ public class Ship extends Sprite {
 
     public Ship(Type type, Direction direction, Vector2 position) {
         super();
+        stateTime = 0.0F;
         this.position = position;
         this.type = type;
         this.direction = direction;
         if (direction == Direction.Horizontal) {
             this.rotation = 90.0F;
         }
+        switch (type){
+            case VerySmall:
+                this.animation =  Assets.Ships.VerySmall();
+        }
         this.texture = new Texture(Gdx.files.internal("Boats/Boat1_water_frame1.png"));
     }
 
     @Override
     public void draw(Batch batch) {
+        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+
+        // Get current frame of animation for the current stateTime
+//        Texture currentFrame = walkAnimation.getKeyFrame(stateTime, true);
         this.setPosition(this.position.x, this.position.y);
         this.setRotation(this.rotation);
         batch.draw(texture, position.x, position.y);
