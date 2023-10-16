5ae1f4cf2cca88b6ee94b6b33d64de5bc6e80606
Samuel LEON <samuel.leon@sfr.fr>
2023-10-16 11:30:22 +0200

    add: game class boilerplate

---
+++ server/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    // Board with the boats of Player 1
+    int[][] BoardPlayerOne = new int [10][10];
+    // Hidden board for attack
+    int[][] BoardAttackPlayerOne = new int [10][10];
+
+    // Board with the boats of Player 2
+    int[][] BoardPlayerTwo = new int [10][10];
+    // Hidden board for attack
+    int[][] BoardAttackPlayerTwo = new int [10][10];
+
+
+}
