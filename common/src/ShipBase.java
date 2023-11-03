1fddcc71fb6bd37ed87006aaa9d4f9c675b4e79d
Yassine AMAGHZAZ <yassine.amaghzaz@gmail.com>
2023-11-03 19:38:39 +0100

    add: common

---
+++ common/src/ShipBase.java
@@ -0,0 +1,16 @@
+public class ShipBase {
+	public enum Direction {
+		Horizontal,
+		Vertical,
+	}
+    public enum Type {
+        VerySmall,
+        Small,
+        Medium,
+        Big
+    }
+	private int size;
+    ShipBase(){
+        
+    }
+}
