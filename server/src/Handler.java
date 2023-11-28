b8d5aa33f95e385b5899308c84b19ca8c5b41f09
Yassine AMAGHZAZ <yassine.amaghzaz@gmail.com>
2023-11-28 14:33:19 +0100

    add: debug symbol

--- server/src/Handler.java
+++ server/src/Handler.java
@@ -11,6 +11,7 @@ import payloads.Session;
 import payloads.Status;
 
 public class Handler {
+	final boolean DEBUG = true;
 	public Json json;
 	public HashMap<String, Session> Sessions; // (sessionID, Session)
 	public HashMap<String, PrintWriter> Users; // (usernames, writer)
@@ -69,7 +70,8 @@ public class Handler {
 	private void MessageHandler(Message request, PrintWriter writer) {
 		// ignores if its not the right session
 		if (!Sessions.containsKey(request.session)) {
-			System.out.println("incorrect session");
+			if (DEBUG)
+				System.out.println("incorrect session");
 			sendStatus(writer, new Status(2));
 			return;
 		}
@@ -83,7 +85,8 @@ public class Handler {
 	private void MissileHandler(Missile request, PrintWriter writer) {
 		// ignores if its not the right session
 		if (!Sessions.containsKey(request.session)) {
-			System.out.println("player: " + request.player + "incorrect session -> ignore missile");
+			if (DEBUG)
+				System.out.println("player: " + request.player + "incorrect session -> ignore missile");
 			sendStatus(writer, new Status(2));
 			return;
 		}
@@ -96,14 +99,17 @@ public class Handler {
 		}
 
 		// assign new turn
-		System.out.println("turn: remove: " + request.player);
+		if (DEBUG)
+			System.out.println("turn: remove: " + request.player);
 		TurnPlayers.remove(request.player);
-		System.out.println("turn: add: " + request.opponent);
+		if (DEBUG)
+			System.out.println("turn: add: " + request.opponent);
 		TurnPlayers.add(request.opponent);
 
 		// update missile
 		Missiles.put(request.session, request);
-		System.out.println("player: " + request.player + " missile accepted");
+		if (DEBUG)
+			System.out.println("player: " + request.player + " missile accepted");
 		// sending corresponding requests
 		// var res = request;
 		// res.opponent = request.player;
@@ -113,81 +119,22 @@ public class Handler {
 
 	private void StatusHandler(Status request, PrintWriter writer) {
 		switch (request.code) {
-			// Session
 			case 4:
-				if (ReadySessions.contains(request.session)) {
-					sendStatus(writer, new Status(1));
-					var s = Sessions.get(request.session);
-					if (request.sender.matches(s.player.username)) {
-						sendSession(writer, s);
-					} else {
-						// reverse roles
-						Session rs = new Session(s.opponent, s.player);
-						rs.type = 2;
-						rs.session = s.session;
-						sendSession(writer, rs);
-					}
-					return;
-				}
-				sendStatus(writer, new Status(3));
-				return;
-			// Missile
+				// Session
+				sessionStatusHanlder(request, writer);
 			case 5:
+				// Missile
 				// sender is able to receive missile attacks
-				if (TurnPlayers.contains(request.sender)) {
-					if (Missiles.containsKey(request.session)) {
-						var missile = Missiles.get(request.session);
-						Missiles.remove(request.session);
-						sendStatus(writer, new Status(1));
-						sendMissile(request.sender, missile);
-						return;
-					}
-					// tell the player that there is a sync error
-					sendStatus(writer, new Status(2));
-					return;
-				}
-				// tell the player to wait for udpate
-				sendStatus(writer, new Status(3));
-				return;
-			// Turn
+				missileStatusHandler(request, writer);
 			case 6:
-				var current = Sessions.get(request.session);
-				var player = current.player.username;
-				var opponent = current.opponent.username;
-				// sender has a turn
-				if (TurnPlayers.contains(request.sender)) {
-					System.out.println("turn: ok " + request.sender);
-					sendStatus(writer, new Status(1));
-				} else
-				// no one has a turn
-				if (!TurnPlayers.contains(player) && !TurnPlayers.contains(opponent)) {
-					System.out.println("turn: giving perm to " + player);
-					TurnPlayers.add(player);
-					if (request.sender.matches(player)) {
-						sendStatus(writer, new Status(1));
-					} else {
-						sendStatus(writer, new Status(3));
-					}
-				} else
-				// Not having a turn
-				{
-					System.out.println("turn: no " + request.sender);
-					sendStatus(writer, new Status(3));
-				}
-
-				// ScoreBoard
+				// Turn
+				turnStatusHandler(request, writer);
 			case 7:
+				// ScoreBoard
 				return;
-			// Chat
 			case 8:
-				if (!Messages.containsKey(request.session)) {
-					sendStatus(writer, new Status(2));
-				} else {
-					var messages = Messages.get(request.session);
-					sendStatus(writer, new Status(1));
-					sendMessages(writer, messages);
-				}
-				return;
+				// Chat
+				chatStatusHandler(request, writer);
 			case 9:
 
 				return;
@@ -195,18 +142,93 @@ public class Handler {
 
 	}
 
+	private void chatStatusHandler(Status request, PrintWriter writer) {
+		if (!Messages.containsKey(request.session)) {
+			sendStatus(writer, new Status(2));
+		} else {
+			var messages = Messages.get(request.session);
+			sendStatus(writer, new Status(1));
+			sendMessages(writer, messages);
+		}
+	}
+
+	private void turnStatusHandler(Status request, PrintWriter writer) {
+		var current = Sessions.get(request.session);
+		var player = current.player.username;
+		var opponent = current.opponent.username;
+		// sender has a turn
+		if (TurnPlayers.contains(request.sender)) {
+			if (DEBUG)
+				System.out.println("turn: ok " + request.sender);
+			sendStatus(writer, new Status(1));
+		} else
+		// no one has a turn
+		if (!TurnPlayers.contains(player) && !TurnPlayers.contains(opponent)) {
+			if (DEBUG)
+				System.out.println("turn: giving perm to " + player);
+			TurnPlayers.add(player);
+			if (request.sender.matches(player)) {
+				sendStatus(writer, new Status(1));
+			} else {
+				sendStatus(writer, new Status(3));
+			}
+		} else
+		// Not having a turn
+		{
+			System.out.println("turn: no " + request.sender);
+			sendStatus(writer, new Status(3));
+		}
+	}
+
+	private void missileStatusHandler(Status request, PrintWriter writer) {
+		if (TurnPlayers.contains(request.sender)) {
+			if (Missiles.containsKey(request.session)) {
+				var missile = Missiles.get(request.session);
+				Missiles.remove(request.session);
+				sendStatus(writer, new Status(1));
+				sendMissile(request.sender, missile);
+				return;
+			}
+			// tell the player that there is a sync error
+			sendStatus(writer, new Status(2));
+			return;
+		}
+		// tell the player to wait for udpate
+		sendStatus(writer, new Status(3));
+	}
+
+	private void sessionStatusHanlder(Status request, PrintWriter writer) {
+		if (ReadySessions.contains(request.session)) {
+			sendStatus(writer, new Status(1));
+			var s = Sessions.get(request.session);
+			if (request.sender.matches(s.player.username)) {
+				sendSession(writer, s);
+			} else {
+				// reverse roles
+				Session rs = new Session(s.opponent, s.player);
+				rs.type = 2;
+				rs.session = s.session;
+				sendSession(writer, rs);
+			}
+			return;
+		}
+		sendStatus(writer, new Status(3));
+	}
+
 	// receives request / sends status codes
 	private void SessionHandler(Session request, PrintWriter writer) {
 		if (!Sessions.containsKey(request.session)) {
 			Sessions.put(request.session, request);
-			System.out.println("session: " + request.session + " set up");
+			if (DEBUG)
+				System.out.println("session: " + request.session + " set up");
 		}
 		var current = Sessions.get(request.session);
 		var username = request.player.username;
 		// registered as player or opponent
 		if (username.matches(current.player.username) || username.matches(current.opponent.username)) {
 			Users.put(request.player.username, writer);
-			System.out.println("player: " + request.player.username + " set up");
+			if (DEBUG)
+				System.out.println("player: " + request.player.username + " set up");
 			sendStatus(writer, new Status(1));
 			return;
 		}
@@ -214,7 +236,8 @@ public class Handler {
 		if (!username.matches(current.player.username) && !username.matches(current.opponent.username)) {
 			// ignores if player is a third player
 			if (Users.containsKey(current.opponent.username)) {
-				System.out.println("player: " + request.player.username + " cannot join session");
+				if (DEBUG)
+					System.out.println("player: " + request.player.username + " cannot join session");
 				sendStatus(writer, new Status(2));
 				return;
 			}
@@ -223,7 +246,8 @@ public class Handler {
 			ReadySessions.add(current.session);
 			Users.put(request.player.username, writer);
 			Messages.put(request.session, new HashSet<>());
-			System.out.println("player: " + request.player.username + " set up");
+			if (DEBUG)
+				System.out.println("player: " + request.player.username + " set up");
 			sendStatus(writer, new Status(1));
 			return;
 		}
